bf95f69d3ea8464923a5ee1339512355c0491d13
CIT142f-36@M307.UC-BCF.EDU.PH <CIT142f-36@M307.UC-BCF.EDU.PH>
2022-09-14 16:51:27 +0800

    Batch Upload

---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo
+pause
+git remote set-url origin https://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@github.com/Chavez88C/School_cit14.git
+git add .
+git commit -m "Batch Upload"
+git push
+pause
